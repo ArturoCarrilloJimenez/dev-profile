d93d113d7f5392adf5c224c458b0c518057aa564
ArturoCarrilloJimenez <arturojimenezalmontaras@gmail.com>
2025-05-21 09:36:59 +0200

    refactor: change section project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,10 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import partytown from '@astrojs/partytown';
 import netlify from '@astrojs/netlify';
-import sitemap from '@astrojs/sitemap';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-import fs from 'node:fs';
 
 import mdx from '@astrojs/mdx';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const directoryPath = path.join(__dirname, 'src', 'content', 'blog');
-const files = fs.readdirSync(directoryPath);
-const siteUrl = 'https://www.michalskorus.pl';
-const blogUrls = files
-	.filter(file => file.includes('md'))
-	.map(file => {
-		const fileName = file.split('.')[0];
-		return `${siteUrl}/blog/${fileName}`;
-	});
+const siteUrl = 'https://www.arturocj.dev';
 
 // https://astro.build/config
 export default defineConfig({
@@ -37,37 +23,6 @@ export default defineConfig({
 			},
 		}),
 		mdx(),
-		sitemap({
-			customPages: [...blogUrls],
-			serialize(item) {
-				if (item.url === 'https://www.michalskorus.pl/') {
-					item.changefreq = 'daily';
-					item.lastmod = new Date();
-					item.priority = 1;
-					return item;
-				}
-
-				if (item.url === 'https://www.michalskorus.pl/blog/') {
-					item.changefreq = 'daily';
-					item.lastmod = new Date();
-					item.priority = 0.8;
-					item.url = 'https://www.michalskorus.pl/blog';
-					return item;
-				}
-
-				if (item.url.includes('blog')) {
-					item.changefreq = 'weekly';
-					item.lastmod = new Date();
-					item.priority = 0.6;
-					return item;
-				}
-
-				item.changefreq = 'weekly';
-				item.lastmod = item.lastmod || new Date();
-				item.priority = 0.3;
-				return item;
-			},
-		}),
 	],
 	markdown: {
 		shikiConfig: {
